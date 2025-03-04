98aa6d8eae6566df3ada514728e6a451abac5eb7
Credypt <jan.erik.grenne@gmail.com>
2025-03-04 14:50:47 +0100

    pwa fixes hopefully

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-setTimeout(() => document.body.classList.add("loaded"), 100)
+setTimeout(() => document.body.classList.add("loaded"), 50)
 
 window.transitionToPage = function(href) {
     document.body.style.opacity = 0;
     sessionStorage.setItem("fade", "true");
     setTimeout(() => { 
         window.location.href = href;
-    }, 500);
-};
+    }, 400);
+};  
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (sessionStorage.getItem("fade") === "true") {
+        document.body.style.opacity = 0;
+        setTimeout(() => {
+            document.body.style.opacity = 1;
+            sessionStorage.removeItem("fade");
+        }, 50);
+    }
+});
 
 const nyhet = "Ny timeplan neste uke";
 
 document.getElementById("nyheter").textContent=nyhet;
 
-let greetings = ["Halla folkens", "Lev livet", "Borre's Nettside", "Nettsiden for ALLE"]
+let greetings = ["Halla folkens", "Lev livet", "Borre's Nettside", "Nettsiden for ALLE", "Gratulerer du er super kul!"]
 let randList = Math.floor(Math.random() * greetings.length)
 
 document.getElementById("welcome-msg").textContent=greetings[randList];
 
-window.transitionToPage = function(href) {
-    document.body.style.opacity = 0;
-    sessionStorage.setItem("fade", "true");
-    setTimeout(() => { 
-        window.location.href = href;
-    }, 500);
-};
-
-    document.addEventListener("DOMContentLoaded", function() {
-        if (sessionStorage.getItem("fade") === "true") {
-            document.body.style.opacity = 0;
-            setTimeout(() => {
-                document.body.style.opacity = 1;
-                sessionStorage.removeItem("fade");
-            }, 50);
-        }
-    });
 
 
-
-    
+document.addEventListener('gesturestart', function (e) {
+    e.preventDefault();
+});
+document.addEventListener('gesturechange', function (e) {
+    e.preventDefault();
+});
+document.addEventListener('gestureend', function (e) {
+    e.preventDefault();
+});
